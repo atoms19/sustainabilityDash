b4f9671742dbcdfc2948599442c475deb8196044
atoms19 <vishalvdevan@gmail.com>
2025-02-14 11:26:29 +0530

    added page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,12 @@
+import { LoginForm } from "@/components/custom/loginForm"
 
-const page = () => {
+
+export default function Page() {
   return (
-    <div>page</div>
+    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
   )
 }
-
-export default page
