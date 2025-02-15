a8690c35fe7168aea8e533f518caed8e5fa726d6
atoms19 <vishalvdevan@gmail.com>
2025-02-15 09:02:01 +0530

    added schema validation and selection schemas

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,2 +1,45 @@
 "use server"
 
+
+
+import { entrySchema, userSchema } from './formSchemas';
+import { insertUser,inSertEntry, insertEntry } from '../db/queries';
+
+export async function handleUserForm(data: FormData) {
+  try {
+    // Convert FormData to an object
+    const formDataObject = Object.fromEntries(data.entries());
+
+    // Parse and validate the data using Zod schema
+    const parsedData = userSchema.parse(formDataObject);
+
+    // Insert the user into the database
+    const newUser = await insertUser(parsedData);
+
+    return { success: true, user: newUser };
+  } catch (error) {
+    return { success: false, error: error.message };
+  }
+}
+
+
+
+export async function handleEntryForm(data: FormData) {
+    try {
+      // Convert FormData to an object
+      const formDataObject = Object.fromEntries(data.entries());
+  
+      // Parse and validate the data using Zod schema
+      const parsedData = entrySchema.parse(formDataObject);
+  
+      // Insert the entry into the database
+    
+      const newEntry = await insertEntry(parsedData);
+  
+      return { success: true, entry: newEntry };
+    } catch (error) {
+      return { success: false, error: error.message };
+    }
+  }
+
+  
